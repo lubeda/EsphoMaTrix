a3f7672b0d97f42003b042b1b96be345f74960e8
LuBeDa <lb@sell-e.de>
2022-02-07 13:22:32 +0100

    coding style

--- components/ehmtx/EHMTX.h
+++ components/ehmtx/EHMTX.h
@@ -7,8 +7,8 @@ const uint8_t MAXQUEUE=16;
 const uint8_t MAXICONS=48;
 const uint8_t TEXTSCROLLSTART=9;
 const uint16_t TICKINTERVAL=1000; // each 1000ms
-#define EHMTX_VERSION "Version: 2022.2.6"
-#define TAG "EHMTX"
+static const char *const EHMTX_VERSION="Version: 2022.2.7";
+static const char *const TAG="EHMTX";
 
 namespace esphome
 {
