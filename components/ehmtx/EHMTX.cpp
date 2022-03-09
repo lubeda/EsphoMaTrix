f211b0339376cba8408a1729b704555e83f1d5fc
LuBeDa <lb@sell-e.de>
2022-03-09 19:18:55 +0100

    typos

--- components/ehmtx/EHMTX.cpp
+++ components/ehmtx/EHMTX.cpp
@@ -314,7 +314,7 @@ namespace esphome
   {
     ESP_LOGCONFIG(TAG, "EspHoMatriX %s", EHMTX_VERSION);
     ESP_LOGCONFIG(TAG, "Icons: %d of %d", this->icon_count, MAXICONS);
-    ESP_LOGCONFIG(TAG, "Offset: x= %d y=f %d", this->xoffset, this->yoffset);
+    ESP_LOGCONFIG(TAG, "Font offset: x=%d y=%d", this->xoffset, this->yoffset);
     ESP_LOGCONFIG(TAG, "Max screens: %d", MAXQUEUE);
     ESP_LOGCONFIG(TAG, "Intervall (ms) scroll: %d anim: %d", this->scroll_intervall, this->anim_intervall);
     ESP_LOGCONFIG(TAG, "Displaytime (s) clock: %d screen: %d", this->clock_time, this->screen_time);
