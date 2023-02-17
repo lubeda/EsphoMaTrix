5e2f3b7f8d6ead5bc122a00b32affa3cdde59c6b
LuBeDa <lb@sell-e.de>
2023-02-17 16:00:29 +0100

    updated version

--- components/ehmtx/EHMTX.h
+++ components/ehmtx/EHMTX.h
@@ -8,7 +8,7 @@ const uint8_t TEXTSCROLLSTART = 8;
 const uint8_t TEXTSTARTOFFSET = (32 - 8);
 
 const uint16_t TICKINTERVAL = 1000; // each 1000ms
-static const char *const EHMTX_VERSION = "Version: 2023.1.0";
+static const char *const EHMTX_VERSION = "Version: 2023.2.0";
 static const char *const TAG = "EHMTX";
 
 namespace esphome
