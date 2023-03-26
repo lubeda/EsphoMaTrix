ea6e83efee6c896b7069ab314a65038252c5bd5c
LuBeDa <lb@sell-e.de>
2023-03-26 19:06:08 +0200

    docu

--- components/ehmtx/EHMTX_screen.cpp
+++ components/ehmtx/EHMTX_screen.cpp
@@ -30,7 +30,7 @@ namespace esphome
 
   void EHMTX_screen::update_screen()
   {
-    if (millis() - this->config_->last_scroll_time >= this->config_->scroll_intervall && this->pixels_ > TEXTSTARTOFFSET)
+    if (millis() - this->config_->last_scroll_time >= this->config_->scroll_interval && this->pixels_ > TEXTSTARTOFFSET)
     {
       this->shiftx_++;
       if (this->shiftx_ > this->pixels_ + TEXTSTARTOFFSET)
@@ -124,7 +124,7 @@ namespace esphome
     this->text = text;
     this->pixels_ = pixel;
     this->shiftx_ = 0;
-    float dd = ceil((this->config_->scroll_count * (TEXTSTARTOFFSET + pixel) * this->config_->scroll_intervall) / 1000);
+    float dd = ceil((this->config_->scroll_count * (TEXTSTARTOFFSET + pixel) * this->config_->scroll_interval) / 1000);
     this->screen_time = (dd > show_time) ? dd : show_time;
     ESP_LOGD(TAG, "display length text: %s pixels %d calculated: %d show_time: %d default: %d", text.c_str(), pixel, this->screen_time, show_time, this->config_->screen_time);
     this->endtime = this->config_->clock->now().timestamp + et * 60;
